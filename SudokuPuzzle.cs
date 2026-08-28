ba194ba39a3aa1e820abd2161d6243ef6e3535de
ronvotri <lengochung28191@gmail.com>
2026-08-28 11:05:48 +0700

    Add Sudoku puzzle model

---
+++ SudokuPuzzle.cs
@@ -0,0 +1,9 @@
+namespace ChuyenTamLinhKoDuaDuocDau;
+
+internal sealed class SudokuPuzzle
+{
+    public string Id { get; set; } = "";
+    public string Difficulty { get; set; } = "Easy";
+    public string Puzzle { get; set; } = "";
+    public string Solution { get; set; } = "";
+}
