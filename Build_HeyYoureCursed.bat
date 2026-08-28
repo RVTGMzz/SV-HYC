2b6a08454fd968b824c4b6ee06506aa8a5622f87
ronvotri <lengochung28191@gmail.com>
2026-08-28 22:50:12 +0700

    Update alpha.8 build banner

--- Build_HeyYoureCursed.bat
+++ Build_HeyYoureCursed.bat
@@ -3,8 +3,8 @@ setlocal
 cd /d "%~dp0"
 
 echo ==========================================
-echo   Hey! You're Cursed! v0.0.6-alpha.7
-echo   TV Target + Portrait + Sudoku Menu Hotfix
+echo   Hey! You're Cursed! v0.0.6-alpha.8
+echo   Controller-first Sudoku Controls
 echo ==========================================
 echo.
 
