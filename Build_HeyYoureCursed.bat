308e6dbd03621cfb8b86a8108cfccc9e0b98d512
ronvotri <lengochung28191@gmail.com>
2026-08-29 13:30:19 +0700

    Update alpha.2.2.3 build label

--- Build_HeyYoureCursed.bat
+++ Build_HeyYoureCursed.bat
@@ -3,8 +3,8 @@ setlocal
 cd /d "%~dp0"
 
 echo ==========================================
-echo   Hey! You're Cursed! v0.0.7-alpha.2.2.2
-echo   Ghost Hover Compile Hotfix + Test Shortcuts
+echo   Hey! You're Cursed! v0.0.7-alpha.2.2.3
+echo   Dialogue Readability Hotfix + Ghost Hover
 echo ==========================================
 echo.
 
