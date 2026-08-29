01fdf63d0149f8f574c79f01d0fe582250f607fc
ronvotri <lengochung28191@gmail.com>
2026-08-30 00:48:42 +0700

    Sync build banner to alpha.2.2.5.4

--- Build_HeyYoureCursed.bat
+++ Build_HeyYoureCursed.bat
@@ -3,8 +3,8 @@ setlocal
 cd /d "%~dp0"
 
 echo ==========================================
-echo   Hey! You're Cursed! v0.0.7-alpha.2.2.5.2
-echo   Vietnamese Progress Labels Hotfix
+echo   Hey! You're Cursed! v0.0.7-alpha.2.2.5.4
+echo   Vietnamese Man Label Hotfix
 echo ==========================================
 echo.
 
